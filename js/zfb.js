ae529e3fa24dd2ea361f10cc1d1d912bee49c8af
石七画 <141410871+2023djash@users.noreply.github.com>
2023-12-07 00:59:14 +0800

    更新 zfb.js

--- js/zfb.js
+++ js/zfb.js
@@ -1,4 +1,15 @@
-var zfb = JSON.parse($response.body);
+/*
+易截图
+
+[rewrite_local]
+^https?:\/\/(jietu6|1jietu)\.com\/apiv2\/user url script-response-body https://raw.githubusercontent.com/2023djash/2023djash/main/js/zfb.js
+
+[MITM]
+hostname = jietu6.com, 1jietu.com
+
+*/
+
+var yjt = JSON.parse($response.body);
 info.group_id = "3";
 info.vip = "1";
 info.group = "3000.01.01";
