3a3ae031eafb9c8d8acc51be20c82f1e765815b1
石七画 <141410871+2023djash@users.noreply.github.com>
2023-12-08 00:50:14 +0800

    更新 555Ad.js

--- js/555Ad.js
+++ js/555Ad.js
@@ -16,21 +16,4 @@ hostname = run.api.qyfxgd.cn, *.weilai555.com, *.ecoliving168.com
 **********************************************/
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 let obj=JSON.parse($response.body);obj.data=obj.data.filter(t=>"advert_self"!==t.layout),obj.data.forEach(t=>{t.list=t.list.filter(t=>3!==t.type)}),$done({body:JSON.stringify(obj)});
